80af205f3ce3a428ed70b9f33a1ede81b82af332
Love Arora <luvarora02@gmail.com>
2026-04-30 14:41:04 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(4+4)    // we are using nodejs, not browser
+
+
+// console.log("hello")  console.log("world")   // gives error
+console.log("hello");  console.log("world")    // no error but readability is low
+
+
+console.log(3
+    +
+    3) // code readability should be high (no error but wrong way)
+
+console.log(3+3)       // right way
+
+
+let name = "love"
+let age = 21
+let isLoggedIn = false
+let state = null
+let userID;
+
+
+// DATATYPES
+
+// number = 2\^53
+// bigInt
+// string ==> ""
+// boolean ==> true/false
+// null ==> standAlone value
+// undefined ==> empty value
+// symbol 
+// object
+
+
+console.log(typeof null)          // object 
+console.log(typeof undefined)    // undefined
+console.log(typeof isLoggedIn)    // boolean
