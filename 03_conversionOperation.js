171f21ba057462d0743b62a7377d8aec6c22319a
Love Arora <luvarora02@gmail.com>
2026-05-15 15:08:38 +0530

    operation

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -57,3 +57,31 @@ let age = "33"
 let valueInObejct = Object(age) 
 console.log(valueInObejct);             // [String: '33']
 console.log(typeof valueInObejct);     // object
+
+
+
+
+/*********************OPERATIONS******************/
+
+let num1 = 3 
+let negValue = -num1
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%4);
+
+
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + 2 + "3" + 4);
+console.log("1" + 2 + 2);
