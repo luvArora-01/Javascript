fd6f638f23f8444d33d72930da7207629eafc374
Love Arora <luvarora02@gmail.com>
2026-05-22 01:16:00 +0530

    switch

---
+++ 04_controlFlow.js/02_switch.js
@@ -0,0 +1,31 @@
+// Switch
+
+    // # syntax:-
+
+    // switch () {
+    //     case value:
+            
+    //         break;
+
+    //     default:
+    //         break;
+    // }
+
+const month = 5
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("Default case matched");
+        break;
+}
